e801d7c8cac5093072e9eb959ba2ca60beb6a058
Sqh3rd <moitz417@gmail.com>
2022-10-09 18:31:42 +0200

    Implementation of argparse

--- Main.py
+++ Main.py
@@ -1,17 +1,34 @@
-import os
+#!/usr/bin/python
+
+import os, argparse
 from Import_Logic import Import_Logic
 from Logger import Logger
 
-logger = Logger("Main Logger")
-imp = Import_Logic(logger)
+if __name__ == "__main__":
+    logger = Logger("Main Logger")
+    imp = Import_Logic(logger)
 
-working_dir_name = input("Name of working dir: ")
+    help_message = "This is a tool to consolidate multiple python files inside a folder to a single python file."
 
-if not os.path.exists(f"./{working_dir_name}/main.py"):
-    logger.error(f"No directory \"{working_dir_name}\" or no file \"main.py\" in \"{working_dir_name}\"", FileNotFoundError)
+    parser = argparse.ArgumentParser(description=help_message)
+    parser.add_argument("-i", "--input", help = "Defines the input folder")
+    parser.add_argument("-o", "--output", help = "Defines the output file")
 
-output_file_name = f"{working_dir_name}_parsed.py"
+    args = parser.parse_args()
 
-open(f"./{output_file_name}", "w").close()
+    working_dir_name = args.input
 
-imp.get_and_cache_imports(f"{working_dir_name}/main.py", output_file_name)
+    if working_dir_name:
+        if not os.path.exists(os.path.join(os.getcwd(), f"{working_dir_name}/main.py")):
+            logger.error(f"No directory \"{working_dir_name}\" or no file \"main.py\" in \"{working_dir_name}\"", FileNotFoundError)
+    else:
+        logger.error(f"No input directory specified!")
+
+    if args.output:
+        output_file_name = os.path.join(os.getcwd(), args.output)
+    else:
+        output_file_name = os.path.join(os.getcwd(), f"{working_dir_name}_parsed.py")
+
+    open(output_file_name, "w").close()
+
+    imp.get_and_cache_imports(os.path.join(working_dir_name, "main.py"), output_file_name)
---
+++ file-compactor.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+from Main import main
+
+if __name__ == "__main__":
+    main()
